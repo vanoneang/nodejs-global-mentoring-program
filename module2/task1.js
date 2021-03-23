d906a11d3ae8aacf0de001a5de6e56d42fb94d3d
vanoneang <525650856@qq.com>
2021-03-23 11:01:57 +0800

    feat: optimize task1 to remove while loop

--- module2/task1.js
+++ module2/task1.js
@@ -1,11 +1,24 @@
-process.stdin.setEncoding('utf8')
+// process.stdin.setEncoding('utf8')
 
-process.stdin.on('readable', () => {
-  let str 
+// process.stdin.on('readable', () => {
+//   let str 
 
-  while ((str = process.stdin.read()) !== null) {
-    const pureStr = str.slice()
-    const reverseStr = pureStr.split('').reverse().join('')
-    process.stdout.write(reverseStr)
-  }
+//   while ((str = process.stdin.read()) !== null) {
+//     const pureStr = str.slice()
+//     const reverseStr = pureStr.split('').reverse().join('')
+//     process.stdout.write(reverseStr)
+//   }
+// })
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+})
+
+rl.on('line', function(line){
+  const pureStr = line.slice()
+  const reverseStr = pureStr.split('').reverse().join('')
+  process.stdout.write(reverseStr)
 })
